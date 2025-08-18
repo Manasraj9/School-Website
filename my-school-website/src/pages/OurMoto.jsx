b4977d8e3840afea64616bb19575710f3d908be7
Manas Raj <manasraj1357@gmail.com>
2025-08-18 12:57:45 +0530

    Add image to Our Moto page and remove About Us page

--- my-school-website/src/pages/OurMoto.jsx
+++ my-school-website/src/pages/OurMoto.jsx
@@ -4,6 +4,13 @@ import Navbar from '../components/Navbar';
 const OurMoto = () => (
   <div>
     <Navbar />
+    <div className="w-full">
+      <img
+        src="/images/Upper.png"
+        alt="Design"
+        className="w-full object-cover"
+      />
+    </div>
     <h1>Our Moto</h1>
     <p>Our school moto content goes here.</p>
   </div>
